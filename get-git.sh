374cfdc6b767ed9a33844f01e3e4fe0bc4be7876
www3838438 <www3838438@yahoo.com>
2018-07-13 18:45:55 +0800

    init

--- get-git.sh
+++ get-git.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
 echo "nanobox-虚拟机管理合"
-git submodule add -f https://github.com/3838438-cc/nanobox.git src/nanobox
+git submodule add -f https://github.com/sbsb3838/nanobox.git src/nanobox
+
 git submodule add -f https://github.com/3838438-cc/nanobox-discourse.git src/nanobox-discourse
+
+
 git submodule add -f https://github.com/sbsb3838/gitkube.git src/gitkube
 
 git submodule add  -f https://github.com/3838438-cc/nanobox-flask.git plugin/nanobox-flask
+git submodule add  -f https://github.com/sbsb3838/yoke.git plugin/yoke
+git submodule add  -f https://github.com/sbsb3838/shaman.git plugin/shaman
+git submodule add  -f https://github.com/sbsb3838/nanobox-engine-golang.git plugin/nanobox-engine-golang
+git submodule add  -f https://github.com/sbsb3838/nanobox-adapter-ubuntu.git plugin/nanobox-adapter-ubuntu
 
 
 git submodule foreach --recursive git checkout master
